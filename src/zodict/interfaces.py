b771962c73263a6c17692e7895d8df4aaee46a84
rnix <rnix@12379f12-61e2-0310-a953-c5ab0a7db1c5>
2009-03-19 07:26:37 +0000

    copyright year currected in module headers
    git-svn-id: https://svn.plone.org/svn/archetypes/AGX/zodict/trunk@10817 12379f12-61e2-0310-a953-c5ab0a7db1c5

--- src/zodict/interfaces.py
+++ src/zodict/interfaces.py
@@ -1,4 +1,4 @@
-# Copyright 2003-2009, Blue Dynamics Alliance - http://bluedynamics.com
+# Copyright 2009, Blue Dynamics Alliance - http://bluedynamics.com
 # GNU General Public Licence Version 2 or later
 
 from zope.interface import Attribute
