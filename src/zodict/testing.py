2f7383635db2594c9cfb18eafa5fdea7b0fc483f
Florian Friesdorf <flo@chaoflow.net>
2010-08-17 16:08:06 +0200

    stub for NodeTester

---
+++ src/zodict/testing.py
@@ -0,0 +1,23 @@
+class NodeTester():
+    """Test all known node behaviour
+    """
+    def __call__(self, node, childs=None):
+        """
+        ``node``
+          the node to test
+        ``childs``
+          odict containing key/val pairs respresenting the child nodespace
+        """
+        self.nodespaces['childs'] = childs
+
+    def _test
+        for 
+        childs.values == [node[x] for x in node]
+
+        
+
+
+        self._test__getitem__(
+
+
+node_tester = NodeTester()
