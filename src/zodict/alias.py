a019f42bab9810d1181c6c4728b88e295e8cda01
Florian Friesdorf <flo@chaoflow.net>
2010-08-17 17:52:44 +0200

    prefixaliaser

---
+++ src/zodict/alias.py
@@ -0,0 +1,16 @@
+
+class PrefixAliaser(object):
+    """An aliaser that prefix all keys
+
+    As it never raise KeyError it is not whitelisting
+    """
+    def __init__(self, prefix=''):
+        self.prefix = prefix
+
+    def __getitem__(self, aliased_key):
+        if not aliased_key.startswith(self.prefix):
+            raise KeyError(u"key '%s' does not match prefix '%s'" % \
+                    (aliased_key, prefix))
+
+    def reverse(self, key):
+        return prefix+key
