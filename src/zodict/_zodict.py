43efdcca621c53eb53e1f9ed627eb43b0365aa4d
Robert Niederreiter <rnix@squarewave.at>
2010-12-18 14:12:24 +0100

    modify deprecation message

--- src/zodict/_zodict.py
+++ src/zodict/_zodict.py
@@ -1,6 +1,11 @@
 from node.utils import (
-    zodict,
     Zodict,
     ReverseMapping,
     AttributeAccess,
 )
+
+zodict = Zodict
+from zope.deprecation import deprecated
+deprecated('zodict', "``zodict`` has been renamed to ``Zodict``. Please "
+                     "modify your code and import to use: "
+                     "``from node.utils import Zodict``")
